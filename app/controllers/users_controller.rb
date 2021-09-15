ac9492d4084ca20ca07da7626aafcf5d0ffa407b
ryohei19 <ryohei.basket@gmail.com>
2021-09-15 17:38:57 +0000

    [user]プロフィール編集機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
   def show
-    @user=User.find(params[:id])
+    @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+       redirect_to user_path(@user)
+    else
+       render :edit
+    end
+  end
+
+
 
   private
   def user_params
