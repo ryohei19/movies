a63b0433c44161f99a90e91e5fff0b462137f9eb
ryohei19 <ryohei.basket@gmail.com>
2021-09-15 19:44:32 +0000

    ルーティング 整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
+
+
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions',
+    passwords: 'admin/passwords',
+    registrations: 'admin/registrations'
+  }
+
+  namespace :admin do
+    resources :genres, except: [:new, :show]
+  end
+
+
   resources :users
+  resources :movies
 
   root 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
