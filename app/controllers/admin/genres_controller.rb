8a947307cf268e75b29ef16892236530736c8ea5
ryohei19 <ryohei.basket@gmail.com>
2021-09-15 20:06:47 +0000

    [admin]ジャンル登録　作成

---
+++ app/controllers/admin/genres_controller.rb
@@ -0,0 +1,18 @@
+class Admin::GenresController < ApplicationController
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_genres_path
+  end
+
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+
+  private
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
+end
