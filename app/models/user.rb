6c7fc44cb8adcdf1f6b3d3f336549a403aa17c1a
ryohei19 <ryohei.basket@gmail.com>
2021-09-17 07:16:35 +0000

    リレーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :movies, dependent: :destroy
+
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
