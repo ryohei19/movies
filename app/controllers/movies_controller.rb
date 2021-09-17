e65f5b96ca4fc4699a330647ae981f1bcd2e0bfb
ryohei19 <ryohei.basket@gmail.com>
2021-09-17 07:17:04 +0000

    [movie]新規投稿機能　作成

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,12 @@ class MoviesController < ApplicationController
     @movie = Movie.new
   end
 
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to user_path(current_user.id)
+  end
+
   private
   def movie_params
   	  params.require(:movie).permit(:name, :star, :review, :spoiler, :user_id, :genre_id)
