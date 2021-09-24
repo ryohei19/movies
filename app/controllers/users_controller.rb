ebca7e7ba3a2b3f9a383f159bf65fde859b2aa95
ryohei19 <ryohei.basket@gmail.com>
2021-09-24 12:06:37 +0000

    [user]ユーザー詳細pで投稿一覧・good一覧をタブ切り替えで閲覧可能に

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,12 @@
 class UsersController < ApplicationController
-  def show #ユーザー詳細・タイムライン・検索
+  def show
     @user = User.find(params[:id])
     @movies = @user.movies
     @followings = Movie.where(user_id: [*current_user.following_ids])
     @genres = Genre.all
     @good = Good.new
-  end
-
-  def movies #ユーザー詳細・投稿一覧表示・検索
-    @user = User.find(params[:id])
-    @movies = @user.movies
-    @followings = Movie.where(user_id: [*current_user.following_ids])
-    @genres = Genre.all
-    @good = Good.new
-  end
-
-  def goods #ユーザー詳細・good一覧・検索
-    @user = User.find(params[:id])
     goods = Good.where(user_id: @user.id).pluck(:movie_id)
     @good_movies = Movie.find(goods)
-    @followings = Movie.where(user_id: [*current_user.following_ids])
-    @genres = Genre.all
-    @good = Good.new
   end
 
   def edit #ユーザー情報編集
