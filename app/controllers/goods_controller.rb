7b1c6825b621d278e98bc1729aa8920ed10a6226
ryohei19 <ryohei.basket@gmail.com>
2021-09-27 11:18:20 +0000

    [good]非同期通信修正

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -3,12 +3,11 @@ class GoodsController < ApplicationController
     @good = current_user.goods.create(movie_id: params[:movie_id])
     @movie = Movie.find(params[:movie_id])
     @movie.create_notification_good!(current_user) #通知
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    @good = Good.find_by(movie_id: params[:movie_id], user_id: current_user.id)
+    @good = Good.find(params[:id])
+    @movie = Movie.find(params[:movie_id])
     @good.destroy
-    redirect_back(fallback_location: root_path)
   end
 end
