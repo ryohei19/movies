c683a5a0b49a3b6d1180d4a2f035a97ffb659c69
ryohei19 <ryohei.basket@gmail.com>
2021-09-28 13:40:45 +0000

    [refile]secret_key記述

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a66b42c0b99fd42df8602641845e518c05757d8950c4b53686ac3391e0b89260ecb4a6a3989552f60674cb0a3ed7841c5e3d75d3878c75fac50d732fadae4b03'
