3b12029b1c8ebbeff516f10b6bc65debb02760d1
ryohei19 <ryohei.basket@gmail.com>
2021-09-28 12:49:50 +0000

    星の表示をjsに記述

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,12 @@
 
 // 追加
 require ('javascripts/jquery_raty')
+
+          $('#star').raty({
+            size     : 36,
+            starOff:  '<%= asset_path('star-off.png') %>',
+            starOn : '<%= asset_path('star-on.png') %>',
+            starHalf: '<%= asset_path('star-half.png') %>',
+            scoreName: 'movie[rate]',
+            half: true,
+          });
