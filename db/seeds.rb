b4aad90d3541114998d33a10e08af8621eff801a
ryohei19 <ryohei.basket@gmail.com>
2021-09-28 14:28:06 +0000

    [genre]seed追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create!(
-                  email: "example@mail.com",
-                  password: "123456",
+Genre.create!(
+                  name: "ロマンス"
                   )
