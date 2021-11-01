06b825ee683aa96e6e64df921546d468ec493bed
ryohei19 <ryohei.basket@gmail.com>
2021-11-01 14:54:48 +0000

    [notification]未読の通知あり時 マークで知らせる

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -8,4 +8,8 @@ module NotificationsHelper
       tag.a(notification.visitor.name, href: user_path(@visitor)) + '　があなたの投稿にいいねしました'
     end
   end
+
+  def unchecked_notifications
+    @notifications = current_user.passive_notifications.where(checked: false)
+  end
 end
