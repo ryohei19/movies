82be4da44be636a8625949fc969317ba8900ba37
ryohei19 <ryohei.basket@gmail.com>
2021-11-03 14:33:37 +0000

    [user]user_status 追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_20_065056) do
+ActiveRecord::Schema.define(version: 2021_11_03_143224) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 2021_09_20_065056) do
     t.string "profile_image_id"
     t.text "introduction"
     t.string "favorite_movie"
+    t.boolean "user_status", default: true, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
